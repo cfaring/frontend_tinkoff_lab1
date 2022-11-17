569832c0c7681c87d5bffe7096b35d0864ede741
Eugene <zarov.gcg@gmail.com>
2022-11-17 16:05:09 +0400

    message

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import 'normalize.css';
 import './index.less';
+
+// const button = document.querySelector('button_scroll');
+// const topic = document.querySelector('.title_for_btn');
+
+// button.addEventListener('click', (e) => {
+//   topic.scrollIntoView({
+//     block: 'start',
+//     behavior: 'smooth',
+//   });
+// });
+
+var hiddenElement = document.getElementById("box");
+var btn = document.querySelector('.button');
+
+function handleButtonClick() {
+   hiddenElement.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+
+btn.addEventListener('click', handleButtonClick);
